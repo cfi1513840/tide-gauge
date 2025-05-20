f918677e16497e3d7777d22ea810bc6b3d1b6253
Ken Howell <cfi1513840@gmail.com>
2025-05-20 16:25:07 -0400

    Completed install.sh script

--- install.sh
+++ install.sh
@@ -1,22 +1,114 @@
-echo "The installation starts with preparation of the secure constants file,"
-echo "which will be used to generate the encrypted constants file used during"
-echo "tide station initialization."
-read -p "Hit return to continue"
-
-if test -f ~/bin/tide_constantsx.json; then
-  echo "An encrypted tide_constants file already exists"
-  read -p "do you want to decrypt and edit the file? Y/N: " answ
-  if [ $answ == "Y" ]; then
-    /usr/bin/python decrypt_constants.py
-    nano tide_constants_clear.json
-    echo "Do you want to overwrite the original file with the one"
-    read -p "that was just edited? Y/N: " answ
-    if [ $answ == "Y" ]; then
-      /usr/bin/python encrypt_constants.py
-      echo "encrypting and writing new constants file"
-    fi
-  fi
+#!/bin/bash
+if test -e tide_constants.tmp; then
+  confound=1
 else
-  cp tide_constants_template.json tide_constants_working.json
-  nano tide_constants_working.json
+  confound=0
 fi
+if test -e /var/www/html/k1; then
+  keyfound=1
+else
+  keyfound=0
+fi
+if test -e /var/www/html/tide_constants.json; then
+  jsonfound=1
+else
+  jsonfound=0
+fi
+echo -e "\e[0mTo prepare for installation, the environment variable file must be edited"
+echo "  to include all installation-specific parameters. note that this will"
+echo "  overwrite an existing tide.env file. This feature can also be"
+echo "  used to allow practice installs to temporary directories for testing."
+echo -e "\e[31m" 
+read -p "Do you want to prepare a new tide.env file? Y/N: " answ
+if [ $answ == "Y" ] || [ $answ == "y" ]; then
+  cp tide_template.env tide_env.tmp
+  nano tide_env.tmp
+fi
+if test -e tide_env.tmp && test -e tide.env; then
+  echo "Warning: the next step will overwrite the existing tide.env file."
+  read -p "Do you want to proceed with the overwrite? Y/N: " answ
+  if [ $answ == "Y" ] || [ $answ == "y" ]; then
+    mv tide_env.tmp tide.env
+  fi
+elif test -e tide_env.tmp; then
+  mv tide_env.tmp tide.env
+fi
+echo
+echo -e "\e[0mThe installation proceeds with the generation of encryption keys and"
+echo "  the preparation of the clear-text version of the constants file,"
+echo "  followed by generation of the encrypted tide_constants.json file."
+echo
+makekeys.py
+echo
+grep "HTML_DIRECTORY" tide.env > grep.tmp
+vari="$(cat grep.tmp)"
+eval htmldir=${vari#*=}
+grep "CGI_DIRECTORY" tide.env > grep.tmp
+vari="$(cat grep.tmp)"
+eval cgidir=${vari#*=}
+echo "HTML files will be copied to ${htmldir}"
+echo "CGI files will be copied to $cgidir"
+echo
+if [ $jsonfound == 1 ]; then
+  echo -e "\e[31mAn encrypted tide_constants.json file already exists."
+  echo "  If you proceed with the installation, all encryption keys"
+  echo "  and the tide_constants.json file will be overwritten,"
+  echo "  thus nullifyin any existing encrypted entries in the database."
+  echo
+  read -p "Do you want to proceed? Y/N: " answ
+  if [ $answ != "Y" ] && [ $answ != "y" ]; then
+    exit
+  fi
+fi
+echo  
+echo -e "\e[0mIf you would like to use your choice of an editor to prepare the"
+echo "  constants file, you can exit this session and edit the file"
+echo "  tide_constants.tmp at your leisure. When editing is complete,"
+echo "  run the install script again to complete the setup process."
+echo -e "\e[31m" 
+read -p "Would you like to exit now to edit the tide_constants.tmp file? Y/N: " answ
+if [ $answ == "Y" ] || [ $answ == "y" ]; then
+  cp tide_constants_template.json tide_constants.tmp
+  exit
+fi
+echo
+echo -e "\e[0mThe constants file will be edited using the nano editor in"
+echo "  clear text format to include all parameters associated with this"
+echo "  tide station implementation. When editing is complete and the file has"
+echo "  been saved, it will be encrypted and saved as ${htmldir}tide_constants.json."
+echo "  Note that no clear text versions of the edited file will be saved."
+echo -e "\e[31m" 
+read -p "Hit return to continue: " go
+echo
+echo -e "\e[0m "
+if test -e tide_constants.tmp; then
+  echo "A clear text version of the tide_constants.tmp file already exists."
+  echo -e "\e[31m"
+  read -p "Do you want to use it to create the encrypted constants file? Y/N: " answ
+  if [ $answ == "Y" ] || [ $answ == "y" ]; then
+    /usr/bin/python encrypt_constants.py tide_constants.tmp
+    echo "encrypting and writing new constants file to /var/www/html/tide_constants.json"
+    mv tide_constants.tmp ${htmldir}tide_constantsx.json
+  fi  
+else
+    cp tide_constants_template.json tide_constants.tmp
+    nano tide_constants.tmp
+    /usr/bin/python encrypt_constants.py tide_constants.tmp
+    echo -e "\e[0mEncrypting and writing new constants file"
+    mv tide_constants.tmp ${htmldir}tide_constantsx.json  
+fi  
+echo "Copying SQLite database core to ${htmldir}tides.db"
+sudo cp sqltides.db ${htmldir}tidesx.db
+sudo cp tide.env ${htmldir}.
+sudo cp *.png ${htmldir}.
+echo "Copying html and cgi files to destination directories"
+sudo cp k* ${htmldir}.
+sudo cp index.html ${htmldir}tide.html
+sudo cp *.html ${htmldir}.
+sudo cp *.cgi ${cgidir}.
+sudo chown www-data ${htmldir}*
+sudo chgrp www-data ${htmldir}*
+sudo chmod 660 ${htmldir}*
+sudo chown www-data ${cgidir}*
+sudo chgrp www-data ${cgidir}*
+sudo chmod 770 ${cgidir}*
