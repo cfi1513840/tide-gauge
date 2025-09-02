123be7bc8cb055297395e5208a915b4d55af8f27
Ken Howell <cfi1513840@gmail.com>
2025-09-02 11:42:14 -0400

    Initial turnover of CubeCell and LoRa 32 script files

---
+++ BEL_S1_Tx_9141MHz_Sf9_Gp4.ino
@@ -0,0 +1,333 @@
+// Changes for V3
+//   KH: response option from RPi to change timing/spacing of sampling, allowing multiple sensors without tx collisions
+//   KH: Change RF Frequency to also separate multiple sensors
+//   DLS: added DEFINEs for TRIGGER_PIN and STATION_ID_NUMBER
+//   DLS: added Maxbotix temperature sensor to data packet, currently just the voltage on the MB temperature input in mV
+//   KH: add default delay logic in the event of packet non-ackknowledgement
+
+#include "LoRaWan_APP.h"
+#include "Arduino.h"
+
+#define TRIGGER_PIN GPIO4
+#define STATION_ID_NUMBER 1
+
+//#define RF_FREQUENCY 915000000  // Hz
+#define RF_FREQUENCY 914100000  // Hz
+
+#define TX_OUTPUT_POWER 18  // dBm
+
+#define LORA_BANDWIDTH 0          // [0: 125 kHz, \
+                                  //  1: 250 kHz, \
+                                  //  2: 500 kHz, \
+                                  //  3: Reserved]
+#define LORA_SPREADING_FACTOR 9  // [SF7..SF12]
+#define LORA_CODINGRATE 1         // [1: 4/5, \
+                                  //  2: 4/6, \
+                                  //  3: 4/7, \
+                                  //  4: 4/8]
+#define LORA_PREAMBLE_LENGTH 8    // Same for Tx and Rx
+#define LORA_SYMBOL_TIMEOUT 0     // Symbols
+#define LORA_FIX_LENGTH_PAYLOAD_ON false
+#define LORA_IQ_INVERSION_ON false
+#define LORA_TX_TIMEOUT 5000
+
+#define RX_TIMEOUT_VALUE 100
+#define BUFFER_SIZE 256      // Define the payload size here
+#define SERIAL1_TIMEOUT 100  //timeout in ms
+
+int timetillwakeup = 38600;
+char txpacket[BUFFER_SIZE];
+char rxpacket[BUFFER_SIZE];
+
+static RadioEvents_t RadioEvents;
+void OnRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr );
+int16_t Rssi, rxSize;
+void OnTxDone(void);
+void OnTxTimeout(void);
+void sleep(void);
+static TimerEvent_t wakeUp;
+
+int16_t txNumber;
+bool sleepMode = false;
+bool rxWait = false;
+bool txSend = false;
+bool txComplete = false;
+bool ackMode = false;
+int rxCount = 0;
+uint8_t packetCount;
+//
+// Preloaded buffer is used for test purposes, overwwritten with actual data for operations
+//
+uint8_t serialBuffer[256] = { "R4090\rR4103\rR0867\rR0868\rR0870\rR4097\rR4089\rR4093\rR4089\rR4089\rR4095\rR0870\rR0870\rR0867\rR0870\rR0870\rR0871\rR0868\rR4090\rR4092\r   " };
+//uint8_t serialBuffer[256] = {"R0870\rR0868\rR0867\rR0868\rR0870\rR0870\rR0870\rR4093\rR0870\rR0870\rR0871\rR0870\rR0870\rR0867\rR0870\rR0870\rR0871\rR0868\rR0870\rR0870\r   "}; // Initialized for testing
+uint8_t spaceChar = 32;
+uint8_t RChar = 82;
+uint8_t DChar = 68;
+int readSize;
+int senbr;
+float sensum;
+float senavg;
+float batv;
+int16_t Uval;
+uint8_t statID = STATION_ID_NUMBER;
+int measurements[20];
+int idx1;
+int idx2;
+int idxm;
+int ridx;
+int thresh;
+int matchCount = 0;
+int newdelay = 0;
+
+void onWakeUp() {
+  //Serial.printf("Waking up\n");
+  sleepMode = false;
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  Serial.print("Initializing\r\n");
+  pinMode(Vext, OUTPUT);
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ADC2, INPUT);
+  pinMode(ADC3, INPUT);
+  digitalWrite(Vext, HIGH);  // Original
+  digitalWrite(TRIGGER_PIN, LOW);
+  TimerInit(&wakeUp, onWakeUp);
+  txNumber = -1;
+  packetCount = 0;
+  sensum = 0;
+  senbr = 0;
+  batv = 0;
+  readSize = 0;
+  sleepMode = false;
+  rxWait = false;
+  rxCount =0;
+  txSend = false;
+  txComplete = false;
+  ackMode = false;
+  RadioEvents.TxDone = OnTxDone;
+  RadioEvents.RxDone = OnRxDone;
+  RadioEvents.TxTimeout = OnTxTimeout;
+  Radio.Init(&RadioEvents);
+  Radio.SetChannel(RF_FREQUENCY);
+  Radio.SetTxConfig(MODEM_LORA, TX_OUTPUT_POWER, 0, LORA_BANDWIDTH,
+                    LORA_SPREADING_FACTOR, LORA_CODINGRATE,
+                    LORA_PREAMBLE_LENGTH, LORA_FIX_LENGTH_PAYLOAD_ON,
+                    true, 0, 0, LORA_IQ_INVERSION_ON, LORA_TX_TIMEOUT);
+  Radio.SetRxConfig(MODEM_LORA, LORA_BANDWIDTH, LORA_SPREADING_FACTOR,
+                    LORA_CODINGRATE, 0, LORA_PREAMBLE_LENGTH,
+                    LORA_SYMBOL_TIMEOUT, LORA_FIX_LENGTH_PAYLOAD_ON,
+                    0, true, 0, 0, LORA_IQ_INVERSION_ON, true );
+}
+
+void loop() {
+  if (sleepMode) {
+    lowPowerHandler();
+  }
+  //
+  // Wait up to 5 seconds following packet transmission for ack from receiver
+  // If ack not received enter sleep mode
+  //
+  if (rxWait) {
+    rxCount++;
+    if (rxCount < 5) {
+      Radio.Rx(0);
+      delay(1000);
+    } else {
+      rxWait = false;
+      rxCount = 0;
+      txNumber = -1;
+      timetillwakeup = 33425;
+      TimerSetValue(&wakeUp, timetillwakeup);
+      TimerStart(&wakeUp);
+      sleepMode = true;
+    }
+  //
+  // Process ack packet if received to establish timing delay
+  //
+  } else if (ackMode) {
+    processAck();
+  //
+  // Inital entry after wakeup, turn on sensor and wait 1 second
+  //
+  } else if (txNumber == -1) {
+    rxCount = 0;
+    digitalWrite(Vext, LOW);
+    delay(500);
+    readSize = Serial1.read(serialBuffer, SERIAL1_TIMEOUT);  // Comment out for testing
+    //Serial.printf("Serial1 init read: %s\r\n", serialBuffer);
+    txNumber++;
+    delay(500);
+    //
+    // Process measurements after 20 samples have been collected at one second intervals
+    //
+  } else if (txNumber == 20) {
+    txNumber++;
+    readSize = Serial1.read(serialBuffer, SERIAL1_TIMEOUT);  // Comment out for testing
+    //Serial.printf("Serial1 read: %s\r\n", serialBuffer);
+    //readSize = 256; // Test only, comment out for operations
+    //Serial.printf("read buffer size: %d\r\n", readSize);
+    idx1 = 0;
+    idx2 = 0;
+    idxm = 0;
+    //
+    // Iterate through the read buffer to extract all of the "R" values
+    //
+    while (idx1 < readSize && serialBuffer[idx1] != spaceChar) {
+      if (serialBuffer[idx1] == RChar) {
+        //Serial.printf("%d ",serialBuffer[idx1]);
+        idx1++;
+        int distance = 0;
+        //
+        // Convert the ascii numeric digits to an integer value
+        //
+        while (idx1 < readSize && serialBuffer[idx1] >= 48 && serialBuffer[idx1] <= 57) {
+          distance = distance * 10;
+          distance = distance + (serialBuffer[idx1] - 48);
+          idx1++;
+        }
+        //
+        // Save measurements while ignoring erroneous values
+        //
+        if (distance == 0 || distance == 9999 || distance == 500) {
+        } else {
+          measurements[idx2] = distance;
+          // Serial.printf("distance: %d\r\n", distance);
+          idxm++;
+          idx2++;
+        }
+      } else {
+        idx1++;
+      }
+    }
+    //Serial.printf("Correlation threshold: %.1f\r\n", idxm*0.60);
+    //
+    // Check correlation of measurements which must exceed 60% to be included in average calculation
+    // Values must be in agreement with 60% of all other measurements within a range of +/- 200mm (7.87")
+    //
+    thresh = floor(idxm * 0.60 + 0.5);
+    // Serial.printf("thresh: %d\r\n", thresh);
+    for (idx1 = 0; idx1 < idxm; idx1++) {
+      matchCount = 0;
+      for (idx2 = 0; idx2 < idxm; idx2++) {
+        if (measurements[idx2] < measurements[idx1] + 200 && measurements[idx2] > measurements[idx1] - 200) {
+          matchCount++;
+        }
+      }
+      if (matchCount >= thresh) {
+        sensum = sensum + float(measurements[idx1]);
+        senbr++;
+      }
+    }
+    //
+    // S - Station ID
+    // V - Battery Voltage
+    // C - Packet counter
+    // R - Ultrasonic Range
+    // M - Number of Correlated values out of the 20 measurements (optional)
+    // s - Solar Panel Millivolts
+    // t - Thermistor Millivolts
+    sprintf(txpacket, "S%d,", statID);
+    //
+    // If the sensor count is non-zero, format and send the measurement message.
+    // Otherwise, send an error message containing the receive buffer contents.
+    //
+    if (senbr != 0) {
+      senavg = (sensum / senbr) + 0.5;
+    } else {
+      senavg = 0;
+    }
+
+    Uval = int(senavg);
+    uint16_t batteryVoltage = getBatteryVoltage();
+    
+    #define R1 470 // kOhms
+    #define R2 1000 // kOhms
+    uint16_t solarVoltage = round(analogReadmV(ADC2) * (R1 + R2) / R1); // Vsolar --- R2 --- aReadmV --- R1----GND
+    //uint16_t solarVoltage = round(analogReadmV(ADC2));
+    uint16_t temperatureVoltage = round(analogReadmV(ADC3));
+
+    sprintf(txpacket + strlen(txpacket), "V%d,", batteryVoltage);
+    sprintf(txpacket + strlen(txpacket), "C%d,", packetCount);
+    sprintf(txpacket + strlen(txpacket), "R%d,", Uval);
+    sprintf(txpacket + strlen(txpacket), "M%d,", senbr);
+    sprintf(txpacket + strlen(txpacket), "s%d,", solarVoltage);
+    sprintf(txpacket + strlen(txpacket), "t%d", temperatureVoltage);
+
+
+    //Serial.printf("Sending Packet %s\n", txpacket);
+    Radio.Send((uint8_t *)txpacket, strlen(txpacket));
+    //
+    // Initialize counters for the next iteration, turn off sensor power and enter sleep mode
+    //
+    packetCount++;
+    if (packetCount == 100) {
+      packetCount = 0;
+    }
+    sensum = 0;
+    senbr = 0;
+  } else {
+    //
+    // Trigger the sensor reading
+    //
+    //Serial.printf("Trigger sensor %d\n", txNumber);
+    digitalWrite(TRIGGER_PIN, HIGH);
+    delay(100);
+    digitalWrite(TRIGGER_PIN, LOW);
+    delay(50);
+    txNumber++;
+    delay(840);
+  }
+}
+void OnTxDone(void) {
+  Radio.Sleep();
+  rxWait = true;
+}
+
+void OnTxTimeout(void) {
+  Radio.Sleep();
+  Serial.print("TX Timeout......");
+}
+
+void OnRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr ) {
+    rxWait = false;
+    Rssi=rssi;
+    rxSize=size;
+    memcpy(rxpacket, payload, size );
+    rxpacket[size]='\0';
+    Radio.Sleep();
+    ackMode = true;
+}
+
+void processAck() {
+    ackMode = false;
+    //Serial.printf("Received: %s,P%d,\r\n",rxpacket,Rssi);
+    digitalWrite(Vext, HIGH); // Turn off sensor
+    delay(300); 
+    ridx = 0;
+    while (ridx < rxSize) {
+      if (rxpacket[ridx] == DChar) {
+        ridx++;
+        newdelay = 0;
+        //
+        // Convert the ascii numeric digits to an integer value
+        //
+        while (ridx < rxSize && rxpacket[ridx] >= 48 && rxpacket[ridx] <= 57) {
+          newdelay = newdelay * 10;
+          newdelay = newdelay + (rxpacket[ridx] - 48);
+          ridx++;
+        }
+      } else {
+        ridx++;
+      }
+    }
+    if (newdelay != 0) {
+      timetillwakeup = newdelay*1000;
+    }
+    txNumber = -1;
+    TimerSetValue(&wakeUp, timetillwakeup);
+    TimerStart(&wakeUp);
+    sleepMode = true;
+}
---
+++ DAM_Rx_915MHz_Sf9.ino
@@ -0,0 +1,103 @@
+/**
+ * Receive LoRa-modulation packets with a sequence number, add RSSI
+ * and output on the serial port.
+ *
+*/
+
+#include <heltec_unofficial.h>
+#include <string>
+
+// Pause between transmited packets in seconds.
+// Set to zero to only transmit a packet when pressing the user button
+// Will not exceed 1% duty cycle, even if you set a lower value.
+#define PAUSE               30
+
+// Frequency in MHz. Keep the decimal point to designate float.
+// Check your own rules and regulations to see what is legal where you are.
+//#define FREQUENCY           866.3       // for Europe
+//#define FREQUENCY           915.0       // for US
+#define FREQUENCY           915.0       // for US
+
+// LoRa bandwidth. Keep the decimal point to designate float.
+// Allowed values are 7.8, 10.4, 15.6, 20.8, 31.25, 41.7, 62.5, 125.0, 250.0 and 500.0 kHz.
+//#define BANDWIDTH           250.0
+#define BANDWIDTH           125.0
+
+// Number from 5 to 12. Higher means slower but higher "processor gain",
+// meaning (in nutshell) longer range and more robust against interference. 
+#define SPREADING_FACTOR    9
+
+// Transmit power in dBm. 0 dBm = 1 mW, enough for tabletop-testing. This value can be
+// set anywhere between -9 dBm (0.125 mW) to 22 dBm (158 mW). Note that the maximum ERP
+// (which is what your antenna maximally radiates) on the EU ISM band is 25 mW, and that
+// transmissting without an antenna can damage your hardware.
+#define TRANSMIT_POWER      18
+
+String rxdata = "test";
+volatile bool rxFlag = false;
+long station = 1;
+long counter = 0 ;
+uint64_t proc_time;
+uint64_t start_time;
+int16_t Rssi;
+char txout[16];
+
+void setup() {
+  start_time = millis()/1000;
+  heltec_setup();
+  Serial.begin(9600);
+  RADIOLIB_OR_HALT(radio.begin());
+  // Set the callback function for received packets
+  radio.setDio1Action(rx);
+  // Set radio parameters
+  RADIOLIB_OR_HALT(radio.setFrequency(FREQUENCY));
+  RADIOLIB_OR_HALT(radio.setBandwidth(BANDWIDTH));
+  RADIOLIB_OR_HALT(radio.setSpreadingFactor(SPREADING_FACTOR));
+  RADIOLIB_OR_HALT(radio.setOutputPower(TRANSMIT_POWER));
+  // Start receiving
+  radio.setDio1Action(rx);
+  RADIOLIB_OR_HALT(radio.startReceive(RADIOLIB_SX126X_RX_TIMEOUT_INF));
+}
+
+void loop() {
+  heltec_loop();
+  // If a packet was received, output it and the RSSI and initiate new read
+  if (rxFlag) {
+    rxFlag = false;
+    radio.readData(rxdata);
+    
+    if (_radiolib_status == RADIOLIB_ERR_NONE) {
+      Rssi = radio.getRSSI();
+      Serial.printf("%s,P%d\n", rxdata.c_str(),Rssi);
+      delay(1000);
+      tx();
+      delay(100);
+      radio.setDio1Action(rx);
+      RADIOLIB_OR_HALT(radio.startReceive(RADIOLIB_SX126X_RX_TIMEOUT_INF));
+    }
+  } else {
+    delay(975);
+  }
+}
+//
+// Send ack packet to sensor with new delay value
+//
+void tx() {
+   proc_time = (millis()/1000 - start_time) % 60;
+    if (rxdata[0] == 'S') {
+     if (rxdata[1] == '1') {
+       station = 1;
+     } else {
+       station = 2;
+     }
+    int delay = ((station*15)-proc_time)+60;
+    sprintf(txout,"T%d,", station);
+    sprintf(txout+strlen(txout),"D%d", delay);
+    radio.clearDio1Action();
+    RADIOLIB(radio.transmit(txout));
+  }
+}
+// Can't do Serial or display things here, takes too much time for the interrupt
+void rx() {
+  rxFlag = true;
+}
