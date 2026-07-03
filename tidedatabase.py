aac7a7182bd477912c962e9d897d07e07d484f93
Ken Howell <cfi1513840@gmail.com>
2026-07-03 15:32:55 -0400

    Always read and process previous 24 hours of tide data

--- tidedatabase.py
+++ tidedatabase.py
@@ -261,7 +261,8 @@ class DbManage:
             f'|> filter(fn:(r) => r._measurement == "{measurement}") '
             f'|> filter(fn: (r) => r.location == "{location}") '
             f'|> filter(fn: (r) => r.sensor_num == "{str(stationid)}") '
-            '|> pivot(rowKey: ["_time"], columnKey: ["_field"], valueColumn: "_value")'
+            '|> pivot(rowKey: ["_time"], columnKey: ["_field"], valueColumn: "_value") '
+            '|> sort(columns: ["_time"], desc: false)'
         )
         tide_list = []
         field_list = []
