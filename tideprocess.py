fb185eafd823a845ab639f865c9785c0d0317c84
Ken Howell <cfi1513840@gmail.com>
2026-07-13 11:40:46 -0400

    Correct sentinel initialization and remove obsolete method

--- tideprocess.py
+++ tideprocess.py
@@ -17,8 +17,8 @@ class ProcTide:
         epochs = []
         self.tide_average = [0 for x in range(0,15)]
         self.last_average = 0
-        self.max_tide = -99
-        self.min_tide = 99
+        self.max_tide = float('-inf')
+        self.min_tide = float('inf')
         new_tide_list = []
         for index, entry in enumerate(self.tide_list):
             new_tide_list.append([entry[0], entry[1], self.trend])
@@ -38,14 +38,14 @@ class ProcTide:
                 if self.average > self.last_average + 0.05:
                     if self.trend == 'low':
                         epochs.append([self.min_tide_time,self.trend])
-                        self.min_tide = 99
-                        self.max_tide = -99                        
+                        self.min_tide = float('inf')
+                        self.max_tide = float('-inf')                        
                     self.trend = 'high'
                 elif self.average < self.last_average - 0.05:
                     if self.trend == 'high':
                         epochs.append([self.max_tide_time,self.trend])
-                        self.min_tide = 99
-                        self.max_tide = -99                            
+                        self.min_tide = float('inf')
+                        self.max_tide = float('-inf')                            
                     self.trend = 'low'
                 self.last_average = self.average
         for index, entry in enumerate(self.tide_list):
@@ -55,53 +55,3 @@ class ProcTide:
 
     def get_tide_list(self):
         return self.tide_list
-        
-    def update_tide_list_old(self, tide_list, tide, tide_time):
-        """
-         Insert new tide measurement into the tide list and
-         check for the occurrence of high or low tide.
-         """
-        from datetime import datetime, timedelta
-        current_time = datetime.now()
-        for index, entry in enumerate(tide_list):
-            if datetime.strptime(
-              entry[0],"%Y-%m-%d %H:%M:%S") >= current_time - timedelta(hours=24):
-                break
-        for findex in range(0,index):
-            tide_list.pop(0)
-        #tide_time = datetime.strftime(current_time, "%Y-%m-%d %H:%M:%S")            
-        tide_list.append([tide_time, tide, ''])
-        self.index += 1
-        if tide > self.max_tide:
-            self.max_tide = tide
-            self.max_tide_time = tide_time
-        if tide < self.min_tide:
-            self.min_tide = tide
-            self.min_tide_time = tide_time
-        self.tide_average = self.tide_average[1:]+[tide]
-        update_time = None
-        if self.index % 15 == 0:
-            self.average = sum(self.tide_average)/len(self.tide_average)
-            if self.average > self.last_average + 0.05:
-                if self.trend == 'low':
-                    update_time = self.min_tide_time
-                    update_trend = self.trend
-                    self.min_tide = 99
-                    self.max_tide = -99                        
-                self.trend = 'high'
-            elif self.average < self.last_average - 0.05:
-                if self.trend == 'high':
-                    update_time = self.max_tide_time
-                    update_trend = self.trend
-                    self.min_tide = 99
-                    self.max_tide = -99                            
-                self.trend = 'low'
-            self.last_average = self.average
-        if update_time:
-            #
-            # Update tide list with high or low tide event
-            #
-            for index, entry in enumerate(tide_list):
-                if entry[0] == update_time:
-                    tide_list[index][2] = update_trend
-        return tide_list
